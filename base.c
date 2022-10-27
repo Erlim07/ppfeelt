db828cac7816b464d4f5919142a195294e02383c
Public <pub@ufu.br>
2022-10-27 10:25:18 -0300

    Primeira aula

---
+++ base.c
@@ -0,0 +1,23 @@
+/*
+*Arquivo: base.c
+*Data criação: 27/out/2022
+*Autor: Euler Borges Ferreira Filho
+*O início do desespero
+*/
+
+#include <stdio.h>
+
+int main(int argc, char** argv){
+	for(int i = 1; i < argc; i++){
+		printf("%s\n", argv[i]);
+	}
+	printf("Prepare-se para o sofrimento!\n");
+
+
+
+
+
+
+
+	return 0;
+} 
