79b2db03bfa09d6313946e8b6ae59e07040d9ec6
Euler <eufilho2004@gmail.com>
2022-11-03 11:20:05 -0300

    imc feito sem teste

--- lab1/imc.c
+++ lab1/imc.c
@@ -13,6 +13,31 @@ int main(void){
     scanf("%d", &peso);
     scanf("%f", &altura);
 
-    float imc = peso/altura;
+    float imc = peso/(altura*altura);
+
+    if(imc<16){
+        printf("%.2f (Perigo de vida\n)", imc);
+    }
+    else if(imc>=16 && imc < 17){
+        printf("%.2f (Muito abaixo do peso\n)", imc);
+    }
+    else if(imc>=17 && imc< 18.5){
+        printf("%.2f (Abaixo do peso\n)", imc);
+    }
+    else if(imc>=18.5 && imc< 25){
+        printf("%.2f (Peso normal)\n", imc);
+    }
+    else if(imc>=25 && imc< 30){
+        printf("%.2f (Acima do peso\n)", imc);
+    }
+    else if(imc>=30 && imc< 35){
+        printf("%.2f (Obesidade Grau I)\n", imc);
+    }
+    else if(imc>=35 && imc< 40){
+        printf("%.2f (Obesidade Grau II)\n", imc);
+    }
+        else if(imc>=30 && imc< 35){
+        printf("%.2f (Obesidade Grau III)\n", imc);
+    }   
     
 }
