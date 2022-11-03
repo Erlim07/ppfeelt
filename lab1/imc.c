6e43d3d54a2e7f261415a1f460b779c36be3d929
Euler <eufilho2004@gmail.com>
2022-11-03 10:50:15 -0300

    principios do imc

---
+++ lab1/imc.c
@@ -0,0 +1,18 @@
+/**
+* Arquivo: aritmInt.c
+* Autor: Euler Borges Ferreira Filho
+* Matricula: 12121EEL001
+* Criado em:03/11/2022
+*/
+
+#include <stdio.h>
+int main(void){
+    int peso;
+    float altura;
+
+    scanf("%d", &peso);
+    scanf("%f", &altura);
+
+    float imc = peso/altura;
+    
+}
