16c6dc8e88a778dde0b43d7b76f7f3ef64e67998
Euler <eufilho2004@gmail.com>
2022-11-03 14:45:04 -0300

    fibonacci em uma linha

---
+++ lab1/fibonacci.c
@@ -0,0 +1,17 @@
+/**
+* Arquivo: fibonacci.c
+* Autor: Euler Borges Ferreira Filho
+* Matricula: 12121EEL001
+* Criado em:03/11/2022
+*/
+
+#include <stdio.h>
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+    for(int i = 0, j = 0, k = 1, t; i < n; i++, t = j+k, j = k, k = t) printf("%d\n", j );
+
+
+    return 0;
+}
