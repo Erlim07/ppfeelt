609eac27f3aa0daeb3ea51602904e4a01c485a46
Euler <eufilho2004@gmail.com>
2022-11-03 11:38:30 -0300

    imc feito, testado e consertado

--- lab1/fatorial.c
+++ lab1/fatorial.c
@@ -1,5 +1,5 @@
 /**
-* Arquivo: aritmInt.c
+* Arquivo: fatorial.c
 * Autor: Euler Borges Ferreira Filho
 * Matricula: 12121EEL001
 * Criado em:03/11/2022
--- lab1/imc.c
+++ lab1/imc.c
@@ -1,5 +1,5 @@
 /**
-* Arquivo: aritmInt.c
+* Arquivo: imc.c
 * Autor: Euler Borges Ferreira Filho
 * Matricula: 12121EEL001
 * Criado em:03/11/2022
@@ -16,19 +16,19 @@ int main(void){
     float imc = peso/(altura*altura);
 
     if(imc<16){
-        printf("%.2f (Perigo de vida\n)", imc);
+        printf("%.2f (Perigo de vida)\n", imc);
     }
     else if(imc>=16 && imc < 17){
-        printf("%.2f (Muito abaixo do peso\n)", imc);
+        printf("%.2f (Muito abaixo do peso)\n", imc);
     }
     else if(imc>=17 && imc< 18.5){
-        printf("%.2f (Abaixo do peso\n)", imc);
+        printf("%.2f (Abaixo do peso)\n", imc);
     }
     else if(imc>=18.5 && imc< 25){
         printf("%.2f (Peso normal)\n", imc);
     }
     else if(imc>=25 && imc< 30){
-        printf("%.2f (Acima do peso\n)", imc);
+        printf("%.2f (Acima do peso)\n", imc);
     }
     else if(imc>=30 && imc< 35){
         printf("%.2f (Obesidade Grau I)\n", imc);
@@ -36,7 +36,7 @@ int main(void){
     else if(imc>=35 && imc< 40){
         printf("%.2f (Obesidade Grau II)\n", imc);
     }
-        else if(imc>=30 && imc< 35){
+        else if(imc>=40){
         printf("%.2f (Obesidade Grau III)\n", imc);
     }   
     
