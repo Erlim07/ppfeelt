81eb6d5a1dadfe237422126fdeec84663131c13d
Euler <eufilho2004@gmail.com>
2022-12-01 10:06:16 -0300

    quais div funcionando legal

--- lab3/quaisDiv.c
+++ lab3/quaisDiv.c
@@ -5,9 +5,32 @@
 * Criado em:01/12/2022
 */
 
-int main(void){
+#include <stdio.h>
 
+int main(void){
+    int N;
+    //pegando N
+    printf("Quantos elementos? ");
+    scanf("%d", &N);
+    int vetor[N];
+    for(int i = 0; i < N; i++){
+        printf("Numero %d: ", i+1);
+        scanf("%d", vetor+i);
+    }
+    //referencia
+    int referencia;
+    printf("Referência: ");
+    scanf("%d", &referencia);
+
+    for(int i = 0; i < N; i++){
+        printf("%d", vetor[i]);
+        if((referencia%vetor[i])==0){
+            printf("#");
+        }
+        printf(" ");
+    }
 
 
     return 1;
 }
+
