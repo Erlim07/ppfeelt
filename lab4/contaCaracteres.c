a08c22ddfaf5e7687d622792490ffeaef193c841
Euler <eufilho2004@gmail.com>
2022-12-09 14:36:21 -0300

    conta caracteres :), palindromo :(

---
+++ lab4/contaCaracteres.c
@@ -0,0 +1,52 @@
+/**
+* Arquivo: palindrono.c
+* Autor: Euler Borges Ferreira Filho
+* Matricula: 12121EEL001
+* Criado em:09/12/2022
+*/
+
+#include <stdio.h>
+void pega_str(char* str, int num);
+#define n 1024
+
+
+int main(void){
+    char frase[n];
+    pega_str(frase, n);
+    int i =0;
+    int letras = 0;
+    int num = 0;
+    int outros = 0;
+
+    while(frase[i]){
+        if((frase[i]<='z' && frase[i]>= 'a') || (frase[i]<='Z' && frase[i]>='A')){
+            letras++;
+        }else if(frase[i]<='9' && frase[i]>='0'){
+            num++;
+        }else{
+            outros++;
+        }
+        i++;
+    }
+    printf("letras: %d  digitos: %d  outros: %d\n", letras, num, outros);
+
+    return 0;
+}
+
+void pega_str(char* str, int num){
+    char c;
+    int i;
+    for(i = 0; i < num; i++){
+        c = getchar();
+        if(c!='\n'){
+            str[i] =c; 
+        }else{
+            str[i] = '\0';
+            break;
+        }
+    }
+    if(i == num){ 
+        str[num-1] = 0;
+        while((c = getchar()) != '\n' && c !=EOF);
+    }
+}
---
+++ lab4/palindrono.c
@@ -0,0 +1,109 @@
+/**
+* Arquivo: palindrono.c
+* Autor: Euler Borges Ferreira Filho
+* Matricula: 12121EEL001
+* Criado em:09/12/2022
+*/
+
+#include <stdio.h>
+void pega_str(char* str, int num);
+#define n 1024
+char* to_lower(char* palavra);
+int lenstr(char* palavra);
+int lenstr_ol(char* palavra);
+void limpa_str(char * str, int tamanho_ef, int tamanho_t, char * str_nova);
+
+
+
+int main(void){
+    char palin[n];
+    printf("Escreva o palindromo: ");
+    //captura a string
+    pega_str(palin, n);
+    //passa pra minúsculas
+    to_lower(palin);
+    // tamanho da string
+    int tamanho = lenstr(palin);
+    //tamanho da string sem sinais(',' entre outros)
+    int tamanho_ef = lenstr_ol(palin);////////*********
+    //retorna a sring sem sinais
+    char palin_limpo[n];
+    limpa_str(palin, tamanho_ef, tamanho, palin_limpo);
+
+    printf("%s\n", *palin_limpo);
+    int sim = 1;
+    for(int i =0; i< tamanho_ef/2; i++){
+        if(!(palin_limpo[i] == palin_limpo[tamanho_ef-1-i])){
+            sim = 0;
+        }
+    }
+    printf("Palindromo: ");
+    if(sim){
+        printf("Verdadeiro\n");
+    }else{
+        printf("Falso\n");
+    }
+    
+
+    return 1;
+}
+
+
+void pega_str(char* str, int num){
+    char c;
+    int i;
+    for(i = 0; i < num; i++){
+        c = getchar();
+        if(c!='\n'){
+            str[i] =c; 
+        }else{
+            str[i] = '\0';
+            break;
+        }
+    }
+    if(i == num){ 
+        str[num-1] = 0;
+        while((c = getchar()) != '\n' && c !=EOF);
+    }
+}
+
+char* to_lower(char* palavra){
+    int i = 0;
+    while(palavra[i]){
+        if(palavra[i]<='Z' && palavra[i] >='A'){
+            palavra[i] -=  ('A'-'a');
+        }
+        i++;
+    }
+
+}
+
+int lenstr(char* palavra){
+    long long int l = 0;
+    while(palavra[l]) l++;
+    return l;
+}
+
+int lenstr_ol(char* palavra){
+    long long int l = 0;
+    int i = 0;
+    while(palavra[i]){
+        if(palavra[i]>='a' && palavra[i]<='z'){
+            l++;
+        }
+        i++;
+    } 
+    return l;
+}
+
+void limpa_str(char * str, int tamanho_ef, int tamanho_t, char * str_nova){
+    int j = 0;
+    for(int i = 0; i < tamanho_t; i++){
+        if(str[i]>='a' && str[i]<='z')
+        str_nova[j] = str[i];
+        printf("%c", str_nova[j]);
+        j++;
+    }
+    str_nova[tamanho_ef-1] = 0;
+    printf("%s", str_nova);
+}
