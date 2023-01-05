080b93347e5ce7347a5f1141778d263934095d09
Euler <eufilho2004@gmail.com>
2023-01-05 13:28:01 -0300

    apoxpi a caminho

---
+++ lab5/aproxpi.c
@@ -0,0 +1,24 @@
+/**
+* Arquivo: aproxpi.c
+* Autor: Euler Borges Ferreira Filho
+* Matricula: 12121EEL001
+* Criado em:05/01/2023
+*/
+
+#include <stdio.h>
+float aproximadora(int n);
+
+
+int main(void){
+    int n;
+    printf("Digite um numero: ")
+    scanf("%d", &n);
+
+
+
+    return 0;
+}
+
+float aproximadora(int n){
+    
+}
